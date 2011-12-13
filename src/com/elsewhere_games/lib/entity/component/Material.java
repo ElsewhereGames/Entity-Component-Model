2185871cdccfacab94609e20bc5acf7fec2072de
Hans Pragt <ElsewhereGames@gmail.com>
2011-12-12 21:45:01 -0800

    Adds a texture definition to the material.

--- src/com/elsewhere_games/lib/entity/component/Material.java
+++ src/com/elsewhere_games/lib/entity/component/Material.java
@@ -2,15 +2,10 @@ package com.elsewhere_games.lib.entity.component;
 
 import com.elsewhere_games.lib.entity.Component;
 import com.elsewhere_games.lib.graphics.Color;
+import com.elsewhere_games.lib.resources.Texture;
 
 public class Material implements Component {
 
-	/*
-	 * Components
-	 */
-	
-	private Color color;
-	
 	/*
 	 * Contructors
 	 */
@@ -19,7 +14,7 @@ public class Material implements Component {
 	 * <p>Class constructor. Initializes the color of this material to black.</p>
 	 */
 	public Material() {
-		this.color = Color.Black;
+		this(Color.Black);
 	}
 	
 	/**
@@ -28,20 +23,32 @@ public class Material implements Component {
 	 * 
 	 * @param color The color of this material.
 	 */
-	public Material(Color color) {
+	public Material(final Color color) {
 		this.color = color;
+	}
+	
+	/**
+	 * <p>Class constructor. Allows the <code>texture</code> of this material to
+	 * be specified.</p>
+	 * 
+	 * @param texture The texture of this material.
+	 */
+	public Material(final Texture texture) {
+		this.texture = texture;
 	}
 	
 	/*
 	 * Accessors and Modifiers
 	 */
 	
+	private Color color;
+	
 	/**
 	 * <p>Gets the color of this material.</p>
 	 * 
 	 * @return The color of this material.
 	 */
-	public Color getColor() {
+	public final Color getColor() {
 		return this.color;
 	}
 	
@@ -50,8 +57,28 @@ public class Material implements Component {
 	 * 
 	 * @param color The new color of this material.
 	 */
-	public void setColor(Color color) {
+	public void setColor(final Color color) {
 		this.color = color;
+	}
+	
+	private Texture texture;
+	
+	/**
+	 * <p>Gets the texture of this material.</p>
+	 * 
+	 * @return The texture of this material.
+	 */
+	public final Texture getTexture() {
+		return this.texture;
+	}
+	
+	/**
+	 * <p>Sets the texture of this material.</p>
+	 * 
+	 * @param texture The new texture of this material.
+	 */
+	public void setTexture(final Texture texture) {
+		this.texture = texture;
 	}
 	
 }
